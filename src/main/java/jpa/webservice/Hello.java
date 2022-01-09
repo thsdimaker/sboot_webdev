c5633e3654d867e88115b9126ffa0a44cdd0319d
DG <thsdimaker@gmail.com>
2022-01-09 21:58:16 +0900

    update

--- src/main/java/jpa/webservice/Hello.java
+++ src/main/java/jpa/webservice/Hello.java
@@ -8,9 +8,9 @@ public class Hello {
 
     private String data;
 
-    /*
-    Lombok 없으면 써야 하는 것들.. 매우 귀찮아짐.
-    * */
+    /**
+    * Lombok 없으면 써야 하는 것들.. 매우 귀찮아짐.
+    **/
 //    public void setData(String data) {
 //        this.data = data;
 //    }
